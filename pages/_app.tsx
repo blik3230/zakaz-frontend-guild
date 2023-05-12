93eaab3e40f116393ad6ada490a0d662224a545e
M_Semenishch <m.semenishch@zakaz.global>
2023-05-12 14:48:54 +0300

    Update GeneralLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import '../styles/globals.css';
+import { CssBaseline } from '@mui/material';
+import type { AppProps } from 'next/app';
+import { Fragment, useEffect } from 'react';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,19 +12,24 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     // Your web app's Firebase configuration
     const firebaseConfig = {
-      apiKey: "AIzaSyBcFNSnyhRbDFbDI4KDtqPDUfCCOBfTwaQ",
-      authDomain: "zakaz-frontend-guild.firebaseapp.com",
-      projectId: "zakaz-frontend-guild",
-      storageBucket: "zakaz-frontend-guild.appspot.com",
-      messagingSenderId: "217750990950",
-      appId: "1:217750990950:web:a86cca0bbec9e21051bda7",
+      apiKey: 'AIzaSyBcFNSnyhRbDFbDI4KDtqPDUfCCOBfTwaQ',
+      authDomain: 'zakaz-frontend-guild.firebaseapp.com',
+      projectId: 'zakaz-frontend-guild',
+      storageBucket: 'zakaz-frontend-guild.appspot.com',
+      messagingSenderId: '217750990950',
+      appId: '1:217750990950:web:a86cca0bbec9e21051bda7',
     };
 
     // Initialize Firebase
     initializeApp(firebaseConfig);
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <CssBaseline />
+      <Component { ...pageProps } />
+    </Fragment>
+  );
 }
 
 export default MyApp;
--- src/layout-components/GeneralLayout/GeneralLayout.tsx
+++ src/layout-components/GeneralLayout/GeneralLayout.tsx
@@ -1,5 +1,9 @@
-import Link from "next/link";
-import { ReactNode } from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button, IconButton, Toolbar, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Link from 'next/link';
+import { ReactNode } from 'react';
 
 interface GeneralLayoutProps {
   children: ReactNode;
@@ -9,21 +13,35 @@ export const GeneralLayout = (props: GeneralLayoutProps) => {
   const { children } = props;
 
   return (
-    <div className="general-layout">
-      <div className="general-layout__header">
-        <header className="header-default">
-          <div className="header-default__left">
-            <Link href="/">
-              <a className="logo">Zakaz Frontend Guild</a>
-            </Link>
-          </div>
-          <div className="header-default__right"></div>
-        </header>
-      </div>
-
-      <div className="general-layout__main">{children}</div>
-
-      <div className="general-layout__footer"></div>
-    </div>
+    <Box>
+      <AppBar position="absolute">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={ { mr: 2 } }
+          >
+            <MenuIcon/>
+          </IconButton>
+          <Link href="/">
+            <Typography href="/" variant="h6" component="a">
+              Zakaz Frontend Guild
+            </Typography>
+          </Link>
+          <Link href="/reviewers">
+            <Typography href="/" variant="h6" component="a" sx={{ml: 2}}>
+              List of reviewers
+            </Typography>
+          </Link>
+          <Button color="inherit" sx={{ml: 'auto'}}>Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Box component="main" sx={{ px: 3 }}>
+        <Toolbar />
+        { children }
+      </Box>
+    </Box>
   );
 };
