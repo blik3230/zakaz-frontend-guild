440dadd5a391428a5ffe8ed1c398ff8eddedeac3
M_Semenishch <m.semenishch@zakaz.global>
2023-05-17 16:27:36 +0300

    Add avatar

--- src/layout-components/GeneralLayout/GeneralLayout.tsx
+++ src/layout-components/GeneralLayout/GeneralLayout.tsx
@@ -1,6 +1,6 @@
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Button, Container, Divider, IconButton, List, Toolbar, Typography } from '@mui/material';
+import { Avatar, Button, Container, Divider, IconButton, List, Toolbar, Typography } from '@mui/material';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
@@ -27,7 +27,6 @@ const whapChildrenSx = {
   minHeight: 0,
 };
 
-
 const drawerWidth: number = 240;
 
 interface AppBarProps extends MuiAppBarProps {
@@ -44,7 +43,7 @@ const AppBar = styled(MuiAppBar, {
   }),
   ...(open && {
     marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
+    width: `calc(100% - ${ drawerWidth }px)`,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
@@ -52,7 +51,8 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+const Drawer = styled(
+  MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
     '& .MuiDrawer-paper': {
       position: 'relative',
@@ -83,19 +83,19 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 export const GeneralLayout = (props: GeneralLayoutProps) => {
   const [open, setOpen] = useState(true);
   const { children } = props;
-  const { isLogedIn, logout } = useAuthService();
+  const { isLogedIn, logout, userProfile } = useAuthService();
 
   const toggleDrawer = () => {
     setOpen(!open);
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={ { display: 'flex' } }>
       <AppBar position="absolute">
         <Toolbar>
           {
             isLogedIn && (
-              <Box sx={{ mr: 2 }}>
+              <Box sx={ { mr: 2 } }>
                 {
                   open ? (
                     <IconButton
@@ -103,9 +103,9 @@ export const GeneralLayout = (props: GeneralLayoutProps) => {
                       edge="start"
                       color="inherit"
                       aria-label="close drawer"
-                      onClick={toggleDrawer}
+                      onClick={ toggleDrawer }
                     >
-                      <ChevronLeftIcon />
+                      <ChevronLeftIcon/>
                     </IconButton>
                   ) : (
                     <IconButton
@@ -113,7 +113,7 @@ export const GeneralLayout = (props: GeneralLayoutProps) => {
                       edge="start"
                       color="inherit"
                       aria-label="open drawer"
-                      onClick={toggleDrawer}
+                      onClick={ toggleDrawer }
                     >
                       <MenuIcon/>
                     </IconButton>
@@ -128,7 +128,7 @@ export const GeneralLayout = (props: GeneralLayoutProps) => {
             variant="h6"
             color="inherit"
             noWrap
-            sx={{ flexGrow: 1 }}
+            sx={ { flexGrow: 1 } }
           >
             Zakaz Frontend Guild
           </Typography>
@@ -145,30 +145,38 @@ export const GeneralLayout = (props: GeneralLayoutProps) => {
             )
           }
 
+          {
+            userProfile && (
+              <Avatar>
+                { `${ userProfile.firstName[0].toUpperCase() }${ userProfile.lastName[0].toUpperCase() }` }
+              </Avatar>
+            )
+          }
+
         </Toolbar>
       </AppBar>
 
       {
         isLogedIn && (
-          <Drawer variant="permanent" open={open}>
+          <Drawer variant="permanent" open={ open }>
             <Toolbar
-              sx={{
+              sx={ {
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'flex-end',
                 px: [1],
-              }}
+              } }
             >
             </Toolbar>
 
-            <List component="nav" sx={(theme) => ({
+            <List component="nav" sx={ (theme) => ({
               [theme.breakpoints.up('sm')]: {
                 pl: 1,
               },
-            })}>
-              {mainListItems}
-              <Divider sx={{ my: 1 }} />
-              {secondaryListItems}
+            }) }>
+              { mainListItems }
+              <Divider sx={ { my: 1 } }/>
+              { secondaryListItems }
             </List>
           </Drawer>
         )
@@ -176,7 +184,7 @@ export const GeneralLayout = (props: GeneralLayoutProps) => {
 
       <Box component="main" sx={ mainSx } id="main">
         <Toolbar/>
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Container maxWidth="lg" sx={ { mt: 4, mb: 4 } }>
           <Box sx={ whapChildrenSx }>
             { children }
           </Box>
